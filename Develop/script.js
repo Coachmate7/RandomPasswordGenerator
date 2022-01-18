5c2e3a088ca0f9c95fe92b5f193ae1f2022ae8b1
joelshewan123@gmail.com <91040579+Coachmate7@users.noreply.github.com>
2022-01-18 21:14:20 +1100

    added password random character generator code

--- Develop/script.js
+++ Develop/script.js
@@ -36,7 +36,8 @@ function generatePassword(){
     passwordLength = parseInt(passwordLength);
     //if conditions are met the user will not be prompted again to choose how many characters
     conditions = true; 
-  }
+    }
+
   }while(!conditions);
 
     //ask the user desired variables for password
@@ -45,10 +46,42 @@ function generatePassword(){
     var symbolChars = confirm("Would you like special characters in your password?");
     var numericChars = confirm("Would you like numbers in your password?")
 
+    //loop counter
     var i;
-    var sectionlength;
 
-if(!upperCaseChars && !lowerCaseChars && !symbolChars && !numericChars){
-  alert("Cannot generate password. you will need atleast one option selected.");
-}
+
+    //prompt telling user must use atleast one of the variables
+    if(!upperCaseChars && !lowerCaseChars && !symbolChars && !numericChars){
+      alert("Cannot generate password. you will need atleast one option selected.");
+    }
+
+    //generate password after atleast one variable has been chosen
+    else{
+
+      //loop will generate the length of password via the deired length entered by the user
+      for(i = 0; i < passwordLength;){
+        //if lower case variable is chosen lower case characters will be included in the password 
+        if(lowerCase){
+          pass = pass.concat(lowerCase.charAt(Math.floor(Math.floor() * lowerCase.length)).toString());
+          i++;
+        }
+        //if upper case variable is chosen upper case characters will be included in the password 
+        if(upperCase && i < passwordLength){
+          pass = pass.concat(upperCase.charAt(Math.floor(Math.floor() * upperCase.length)).toString());
+          i++;
+        }     
+        //if symbol variable is chosen symbol characters will be included in the password 
+        if(symbols && i < passwordLength){
+          pass = pass.concat(symbols.charAt(Math.floor(Math.floor() * symbols.length)).toString());
+          i++;
+        }
+        //if numeric variable is chosen numeric characters will be included in the password 
+        if(numeric && i < passwordLength){
+          pass = pass.concat(numeric.charAt(Math.floor(Math.floor() * numeric.length)).toString());
+          i++;
+      }
+    }
+  }
+  //returns the generated password to the console
+  return pass;
 }
