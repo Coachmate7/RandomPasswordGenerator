513ff691167c0350df2e5d5136521db539ccfb31
joelshewan123@gmail.com <91040579+Coachmate7@users.noreply.github.com>
2022-01-18 20:44:37 +1100

    character length prompt added

--- Develop/script.js
+++ Develop/script.js
@@ -28,4 +28,13 @@ function generatePassword(){
   //length of password set by the user
   var passwordLength;
 
+  do{
+    //pasword length prompt for users desired length
+    passwordLength = prompt("How long would you like your password to be? Must be between 8 and 128 characters.");
+    //user decides how many number of characters desiganted for the password
+    if(parseInt(passwordLength)!=NaN && passwordLength >= 8 && passwordLength <= 128) {
+    passwordLength = parseInt(passwordLength);
+    //if conditions are met the user will not be prompted again to choose how many characters
+    conditions = true; 
+  }
 }
